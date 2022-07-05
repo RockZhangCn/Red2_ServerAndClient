5de0d1c77c479a2828cdee47aeff0cc340803adb
rock.zhang.cn <rock.zhang.cn@gmail.com>
2022-07-05 20:43:17 +0800

    specify differrent poker type

--- common/card.py
+++ common/card.py
@@ -13,18 +13,6 @@ class Card(object):
         return value
 
     @staticmethod
-    def card_to_picture_index(card):
-        role = card.split("*")[0]
-        value = card.split("*")[1]
-        role = int(role)
-        index = Card.cards_index.index(value)
-        return index * 4 + role
-
-    @staticmethod
-    def cards_to_string(cards):
-        return "|".join(cards)
-
-    @staticmethod
     def card_compare(card1_name, card2_name):
         value1 = Card.adjust_value(card1_name)
         value2 = Card.adjust_value(card2_name)
@@ -35,84 +23,78 @@ class Card(object):
         else:
             return 0
 
-    def __str__(self):
-        return self.__name
-
-
 class CardHand(object):
-    MODE_SINGLE = "single"
-    MODE_PAIR = "pair"
-    MODE_THREE = "three"
-    MODE_THREE_ONE = "three1"
-    MODE_THREE_TWE = "three2"
-    MODE_AIRPLANE_NONE = MODE_THREE + "s"  # 2*3
-    MODE_AIRPLANE_ONE = MODE_THREE_ONE + "s"  # 2*4
-    MODE_AIRPLANE_TWE = MODE_THREE_TWE + "s"  # 2*5
-    MODE_SINGLE_LONG = "single_long"  # 6
-    MODE_PAIR_LONG = "pair_long"  # 6
+    MODE_SINGLE = 1 # "single"
+    MODE_PAIR = 2 #"pair"
+    MODE_THREE = 3 #"three"
+    MODE_THREE_ONE = 4 # "three1"  8883
+    MODE_THREE_TWE = 5 #"three2"   88833
+    MODE_AIRPLANE_NONE = 6 # MODE_THREE + "s"  # 2*3  555666
+    MODE_AIRPLANE_ONE = 7 # MODE_THREE_ONE + "s"  # 2*4  55576668
+    MODE_AIRPLANE_TWE = 8 #MODE_THREE_TWE + "s"  # 2*5   5557766688
+    MODE_SINGLE_LONG = 9 # "single_long"  # 6   345678
+    MODE_PAIR_LONG = 10 # "pair_long"  # 6    334455
 
-    MODE_BOMB = "bombs"  # 4
-    MODE_TWO_RED2 = "twored2"  # 2
-    MODE_INVALID = "invalid"
+    MODE_BOMB = 11 # "bombs"  # 4   3333
+    MODE_TWO_RED2 = 12 # "king2"  # 2   22
+    MODE_INVALID = -1 #"invalid"
 
-    def __init__(self, cards):
-        self.cards_in_a_hand = cards
-        self.cards_in_a_hand.sort(key=Card.adjust_value, reverse=True)
+    @staticmethod
+    def value(self, cards):
+        cards.sort(reverse=True)
 
-    def hand_mode(self):
-        cnt = len(self.cards_in_a_hand)
         value_set = set()
-        for cardname in self.cards_in_a_hand:
-            value_set.add(Card.adjust_value(cardname))
+        for card in cards:
+            value_set.add(Card.adjust_value(card))
 
+        cnt = len(cards)
         value_set_len = len(value_set)
         if cnt == 1:
             return CardHand.MODE_SINGLE
-        if cnt == 2:
-            if value_set_len == 1:
-                if (Card.adjust_value(self.cards_in_a_hand[0]) == 15) and (self.cards_in_a_hand[0][0:2] == "♥️") and (
-                        self.cards_in_a_hand[1][0:2] == "♥️"):
-                    return CardHand.MODE_TWO_RED2
-                else:
-                    return CardHand.MODE_PAIR
+        elif cnt == 2 and value_set_len == 1:
+            if cards == [48, 48]:
+                return CardHand.MODE_TWO_RED2
             else:
-                return CardHand.MODE_INVALID
-        if cnt == 3:
-            if value_set_len == 3:
-                return CardHand.MODE_THREE
-            else:
-                return CardHand.MODE_INVALID
+                return CardHand.MODE_PAIR
 
-        if cnt == 4:
+        elif cnt == 3 and value_set_len == 3:
+            return CardHand.MODE_THREE
+
+        elif cnt == 4:
             if value_set_len == 2:
+                # TODO
                 return CardHand.MODE_THREE_ONE
             elif value_set_len == 1:
                 return CardHand.MODE_BOMB
             else:
                 return CardHand.MODE_INVALID
 
-        if cnt == 5:
+        elif cnt == 5:
             if value_set_len == 2:
+                # TODO more detail
                 return CardHand.MODE_THREE_TWE
             elif value_set_len == 1:
                 return CardHand.MODE_BOMB
             else:
                 return CardHand.MODE_INVALID
 
-        if cnt >= 6:
+        elif cnt >= 6:
             if value_set_len == cnt:
                 return CardHand.MODE_SINGLE_LONG
             if value_set_len == cnt // 2:
+                # TODO more detail
                 return CardHand.MODE_PAIR_LONG
             if value_set_len == cnt // 3:
                 return CardHand.MODE_AIRPLANE_NONE
             if value_set_len == 1:
                 return CardHand.MODE_BOMB
-            if cnt % 5 == 0 and value_set_len == (cnt // 5) * 2:
+            if cnt % 5 == 0 and cnt//5 <= value_set_len == (cnt // 5) * 2:
                 return CardHand.MODE_AIRPLANE_TWE
-            if cnt % 4 == 0 and value_set_len <= (cnt // 4) * 2:
+            if cnt % 4 == 0 and cnt//4 <= value_set_len <= (cnt // 4) * 2:
                 return CardHand.MODE_AIRPLANE_ONE
 
+            return CardHand.MODE_INVALID
+        else:
             return CardHand.MODE_INVALID
 
 
